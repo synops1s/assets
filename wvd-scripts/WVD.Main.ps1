0dd7774d8b55d9b0de19c70fc85af3e75e4d50f0
Douwstra <oscar.douwstra@fujitsu.com>
2020-11-27 21:27:55 +0100

    .

--- wvd-scripts/WVD.Main.ps1
+++ wvd-scripts/WVD.Main.ps1
@@ -2,10 +2,10 @@ New-Item -Path "C:\WVD" -ItemType Directory -ErrorAction SilentlyContinue -Force
 
 Start-Transcript -Path "C:\WVD\WVD.Main.log"
 
-Start-Process -NoNewWindow powershell '-File WVD.Initialize.ps1' -Verbose -Wait
 Start-Process -NoNewWindow powershell '-File WVD.FSLogix.Unpack.ps1' -Verbose -Wait
 Start-Process -NoNewWindow powershell '-File WVD.FSLogix.Install.ps1' -Verbose -Wait
 Start-Process -NoNewWindow powershell '-File WVD.OneDrive.ps1' -Verbose -Wait
+Start-Process -NoNewWindow powershell '-File WVD.AccessControlLists.ps1' -Verbose -Wait
 Start-Process -NoNewWindow powershell '-File WVD.Registration.ps1' -Verbose -Wait
 
 Stop-Transcript
