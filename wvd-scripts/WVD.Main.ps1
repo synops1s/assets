42d40806d2c574d163a8c83f656cca8e456d4c76
Douwstra <oscar.douwstra@fujitsu.com>
2020-12-30 14:50:35 +0100

    .

--- wvd-scripts/WVD.Main.ps1
+++ wvd-scripts/WVD.Main.ps1
@@ -1,5 +1,5 @@
 param(
-    [String]$BasePath
+    [String]$BasePath = "."
 )
 
 Function Invoke-Script {
@@ -14,17 +14,26 @@ Function Invoke-Script {
     $PSFilePath = Join-Path -Path $BasePath -ChildPath $FilePath 
     $PSFileName = [System.IO.Path]::GetFileNameWithoutExtension($PSFilePath)
 
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass", "-File $($PSFilePath) -BasePath $($BasePath)" -RedirectStandardError "C:\WVD\$($PSFileName).RSE.log" -Wait -Verbose
+    Write-Host $PSFilePath
+
+    if($True -eq [System.IO.File]::Exists($PSFileName))
+    {
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass", "-File $($PSFilePath) -BasePath $($BasePath)" -RedirectStandardError "C:\WVD\$($PSFileName).RSE.log" -Wait -Verbose
+    }
+    else
+    {
+        Write-Information "File '$($PSFilePath)' does not exists"
+    }
 }
 
 New-Item -Path "C:\WVD" -ItemType Directory -Force
 
 Start-Transcript -Path "C:\WVD\WVD.Main.log" -Force
 
-Invoke-Script -FileName "WVD.FSLogix.Unpack" -BasePath "."
-Invoke-Script -FileName "WVD.FSLogix.Install" -BasePath "."
-Invoke-Script -FileName "WVD.FSLogix.Config" -BasePath "."
-Invoke-Script -FileName "WVD.Registration" -BasePath "."
+Invoke-Script -FileName "WVD.FSLogix.Unpack.ps1"
+Invoke-Script -FileName "WVD.FSLogix.Install.ps1"
+Invoke-Script -FileName "WVD.FSLogix.Config.ps1"
+Invoke-Script -FileName "WVD.Registration.ps1"
 
 Invoke-Script -FilePath "WVD.Apps.ps1" -BasePath $BasePath
 
