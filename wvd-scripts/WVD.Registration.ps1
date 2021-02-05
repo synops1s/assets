c13c13a0aed432516ef99694ff52f93028ceff1c
Douwstra <oscar.douwstra@fujitsu.com>
2021-02-05 11:32:00 +0100

    .

--- wvd-scripts/WVD.Registration.ps1
+++ wvd-scripts/WVD.Registration.ps1
@@ -6,6 +6,7 @@ $BootloaderFileName = "Microsoft.RDInfra.RDAgentBootLoader.msi"
 
 $RegistrationToken = Get-Content -Path "WVD.Registration.*.token"
 
+Start-Process -FilePath "gpupdate.exe" -ArgumentList "/target:computer" -NoNewWindow -Wait
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $AgentFileName", "/quiet", "/qn", "/norestart", "/passive", "REGISTRATIONTOKEN=$RegistrationToken", "/l* C:\WVD\WVD.Registration.Agent.log" -NoNewWindow -Wait -ErrorAction Continue -Verbose
 Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $BootloaderFileName", "/quiet", "/qn", "/norestart", "/passive", "/l* C:\WVD\WVD.Registration.Bootloader.log" -NoNewWindow -Wait -ErrorAction Continue -Verbose
 
