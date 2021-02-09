3ae109433d38f34ab8c939bad1f63d111dcd9f55
Douwstra <oscar.douwstra@fujitsu.com>
2021-02-09 19:15:21 +0100

    .

--- wvd-scripts/WVD.SSO.Office.ps1
+++ wvd-scripts/WVD.SSO.Office.ps1
@@ -17,4 +17,11 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Name "KFMSi
 
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "OneDrive " -Value "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" -PropertyType String -Force -Verbose
 
+if($False -eq (Test-Path -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RailRunonce"))
+{
+    New-Item -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RailRunonce" -Force -Verbose
+}
+
+New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\RailRunonce" -Name "OneDrive" -Value "C:\\Program Files (x86)\\Microsoft OneDrive\\OneDrive.exe /background" -PropertyType String -Force -Verbose
+
 Stop-Transcript
