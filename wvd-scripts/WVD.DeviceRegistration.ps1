899d787ae0f9217a1c8bff2924bef1ee886beae8
Douwstra <oscar.douwstra@fujitsu.com>
2021-02-11 11:10:38 +0100

    .

--- wvd-scripts/WVD.DeviceRegistration.ps1
+++ wvd-scripts/WVD.DeviceRegistration.ps1
@@ -16,4 +16,7 @@ if($False -eq (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CDJ\AAD" -Name "TenantId" -Value $TenantId -PropertyType String -Force -Verbose
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CDJ\AAD" -Name "TenantName" -Value $TenantName -PropertyType String -Force -Verbose
 
+### Just trigger the Automatic-Device-Join task which will set the (userCertificate) Attribute in AD (Which allows AAD Connect Filter to sync device to AAD) ###
+Start-Process -FilePath "$($env:SystemRoot)\System32\dsregcmd.exe" -ArgumentList "/join", "/debug" -NoNewWindow -Wait
+
 Stop-Transcript
