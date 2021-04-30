cb8595b558ce957ab4d23e6a1baa1e33ea0209d6
Douwstra <oscar.douwstra@fujitsu.com>
2021-04-30 20:19:58 +0200

    .

--- wvd-apps/Scripts/WVD.Apps.Aquarius.ps1
+++ wvd-apps/Scripts/WVD.Apps.Aquarius.ps1
@@ -1,13 +1,55 @@
-$BasePath = "C:\WVD.Unpack"
+param (
+    [Parameter(Mandatory=$true)]
+    [String]
+    [ValidateNotNullOrEmpty()]
+    $BasePath,
 
-$Scripts = @(
-    "WVD.Apps.Aquarius.Unpack.ps1"
-    "WVD.Apps.Aquarius.Install.ps1"
-    "WVD.Apps.Aquarius.Config.ps1"
+    [Parameter(Mandatory=$true)]
+    [String]
+    [ValidateNotNullOrEmpty()]
+    $InstallPath,
+    
+    [Parameter(Mandatory=$true)]
+    [String]
+    [ValidateNotNullOrEmpty()]
+    $UnpackPath
 )
 
-$Scripts | Foreach-Object {
+New-Item -Path $InstallPath -ItemType Directory -ErrorAction SilentlyContinue -Force -Verbose
 
-    $FilePath = Join-Path -Path $BasePath -ChildPath $_
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Unrestricted", "-File $($FilePath)" -RedirectStandardError "C:\WVD\$($FilePath).RSE.log" -Wait -Verbose
+$VCRedistFilePath = Join-Path -Path $UnpackPath -ChildPath "vcredist_x64.exe"
+$JavaSEFilePath = Join-Path -Path $UnpackPath -ChildPath "jdk11.0.464.msi"
+$TransformFilePath = Join-Path -Path $UnpackPath -ChildPath "Oracle-JavaSEDevelopmentKit_11.0.4_x64_EN_1.1.1.mst"
+
+try {
+
+    Start-Process -FilePath $VCRedistFilePath -ArgumentList "/install", "/quiet", "/norestart" -NoNewWindow -Wait -ErrorAction Continue -Verbose
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i $($JavaSEFilePath)", "/quiet", "/qb-!", "/norestart", "TRANSFORM=$($TransformFilePath)", "/l* $($JavaSEFilePath).log" -NoNewWindow -Wait -ErrorAction Continue -Verbose
+    
+    $Result = [System.Environment]::ExitCode
 }
+catch {
+    $Result = [System.Environment]::Exitcode
+}
+
+$Result
+
+#region Aquarius
+$CmdAquarius = @"
+@echo off
+IF EXIST "C:\Program Files\Java\jdk-11.0.4\bin\java.exe" (
+    set JAVA_HOME=C:\Program Files\Java\jdk-11.0.4
+)
+
+\\PNWNLOTA079\aquarius_519\sw_runtime\Aquarius.cmd
+"@
+$CmdAquarius | Set-Content -Path "$($InstallPath)\Aquarius.cmd" -Force -Verbose
+#endregion
+
+#region Emacs
+$CmdEmacs = @"
+@echo off
+\\PNWNLOTA079\aquarius_519\sw_runtime\Aquarius_emacs.cmd
+"@
+$CmdEmacs | Set-Content -Path "$($InstallPath)\Emacs.cmd" -Force -Verbose
+#endregion
